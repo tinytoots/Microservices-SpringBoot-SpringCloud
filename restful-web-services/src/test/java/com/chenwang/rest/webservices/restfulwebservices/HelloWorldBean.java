b1e9402d7ab63c71d3ec0d333211839435597a32
tinytoots <wangchen2018n@163.com>
2021-08-13 13:43:39 -0400

    add new proj

---
+++ restful-web-services/src/test/java/com/chenwang/rest/webservices/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,21 @@
+package com.chenwang.rest.webservices.restfulwebservices;
+
+public class HelloWorldBean {
+
+    private String message;
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
