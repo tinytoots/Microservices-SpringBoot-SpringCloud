d45efbdc1b8451174892c4ac5e7bb9633dbccd57
tinytoots <wangchen2018n@163.com>
2021-08-14 19:20:22 -0400

    test

---
+++ restful-web-services2/src/main/java/com/chenwang/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,13 @@
+package com.chenwang.rest.webservices.restfulwebservices.user;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+
+    // retrieveAllUsers
+
+    // retrieveUser(int id)
+
+
+}
