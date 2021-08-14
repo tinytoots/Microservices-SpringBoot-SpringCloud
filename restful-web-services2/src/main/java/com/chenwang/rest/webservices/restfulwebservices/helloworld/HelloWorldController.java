0301eee7a70019fc18a5012d7b98efe346dbb6e9
tinytoots <wangchen2018n@163.com>
2021-08-13 21:50:29 -0400

    test

--- restful-web-services2/src/main/java/com/chenwang/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services2/src/main/java/com/chenwang/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
-package com.chenwang.rest.webservices.restfulwebservices;
+package com.chenwang.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,10 +12,15 @@ public class HelloWorldController {
         return "Hello World";
     }
 
-    // hello-world-bean
+    // hello-world-bean 自动转和返回json
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
 }
